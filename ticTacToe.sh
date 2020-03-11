abf27e17ad0ef75399cb848426eb62932adf95e7
Prithvi0 <prithviy96@gmail.com>
2020-03-11 18:22:52 +0530

    Added program to assign a letter to player

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -3,11 +3,26 @@ echo "Welcome to TicTacToe"
 # ARRAY DECLARATION
 declare -a defaultBoard
 
-# START FRESH WITH RESETTING THE BOARD
+# CONSTANTS
+letter1=0
+letter2=1
+
+# FUNCTION TO RESET THE BOARD
 function boardReset () {
 	for((element=0;element<=8;element++ ))
 	do
 		defaultBoard[$element]=" "
 	done
 }
-boardReset
+boardReset	# CALLING THE FUNCTION
+
+# CONDITION TO ASSIGN A LETTER TO PLAYER
+letterAssign=$((RANDOM%2))
+if [[ $letterAssign -eq $letter1 ]]
+then
+	playerLetter="X"
+elif [[ $letterAssign -eq $letter2 ]]
+then
+	playerLetter="O"
+fi
+
