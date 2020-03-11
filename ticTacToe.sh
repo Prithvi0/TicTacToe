e5df8379901480eea4213a049b9704d186868d53
Prithvi0 <prithviy96@gmail.com>
2020-03-11 18:12:35 +0530

    Added program to start with resetting the board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,2 +1,13 @@
 #!/bin/bash -x
 echo "Welcome to TicTacToe"
+# ARRAY DECLARATION
+declare -a defaultBoard
+
+# START FRESH WITH RESETTING THE BOARD
+function boardReset () {
+	for((element=0;element<=8;element++ ))
+	do
+		defaultBoard[$element]=" "
+	done
+}
+boardReset
